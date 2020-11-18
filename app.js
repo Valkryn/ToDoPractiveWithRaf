8fc1c72a0f38aee2fd18f6bfee5175ef1d97663c
Valkryn <calvinraphaelg@gmail.com>
2020-11-17 19:28:28 -0500

    Mongo added

--- app.js
+++ app.js
@@ -1,31 +1,86 @@
-const express = require ("express")
-const bodyparser = require ("body-parser")
+//jshint esversion:6
 
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require ("mongoose")
 
-let items = []
+const app = express();
 
-app.use(bodyparser.urlencoded({extended: true}))
-app.use(express.static("public"))
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs');
 
-app.get("/",function(req,res){
-  let today = new Date()
-  let options = {
-    weekday:"long",
-    day:"numeric",
-    month:"long"
-  }
-  let day = today.toLocaleDateString("en-US",options)
-  res.render("list",{kindOfDay:day , newListItems:items})
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser:true})
+
+const itemsSchema = {
+  name : String
+}
+
+const Item = mongoose.model("Item",itemsSchema)
+
+const item1 = new Item ({
+  name:"Welcome to the list"
 })
 
-app.post("/",function(req,res){
-  let item = req.body.newItem
-  items.push(item)
+const item2 = new Item ({
+  name:"hit + to add a new item"
+})
+
+const item3 = new Item ({
+  name:"<-- Hit this to delete an Item"
+})
+
+const defaultItems  = [item1,item2,item3];
+
+app.get("/", function(req, res) {
+
+  Item.find({},function(err,foundItems){
+    if (foundItems.length === 0){
+      Item.insertMany(defaultItems,function(err){
+        if (err){
+          console.log(err)
+        } else {
+          console.log("sucess")
+        }
+      })
+      res.redirect("/")
+    }else {
+        res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  })
+});
+
+app.post("/", function(req, res){
+
+  const itemName = req.body.newItem;
+  const item = new Item ({
+    name:itemName
+  })
+  item.save()
   res.redirect("/")
+});
+
+app.post("/delete", function(req,res){
+  const checkedItem = req.body.checkbox
+  Item.deleteOne({_id:checkedItem}, function(err){
+    if (err){
+      console.log(err)
+    } else {
+      console.log("deleted")
+    }
+    res.redirect("/")
+  })
 })
 
-app.listen(3000, function(){
-  console.log("server is active")
-})
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
